4e3731beca27aa93670c9539e0fe19dfac27aa94
bome24 <81296575+bome24@users.noreply.github.com>
2022-04-03 23:16:29 +0900

    connecting outlet

---
+++ WeatherForecast/Cell/SummaryTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SummaryTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by BoMin on 2022/04/03.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    static let identifier = "SummaryTableViewCell"
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
