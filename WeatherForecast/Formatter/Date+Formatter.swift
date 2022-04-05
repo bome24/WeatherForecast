f5c684b05694101febdec34c41ad88453d06209b
bome24 <81296575+bome24@users.noreply.github.com>
2022-04-06 00:01:00 +0900

    Forecast list

---
+++ WeatherForecast/Formatter/Date+Formatter.swift
@@ -0,0 +1,26 @@
+//
+//  Date+Formatter.swift
+//  WeatherForecast
+//
+//  Created by BoMin on 2022/04/05.
+//
+
+import Foundation
+
+fileprivate let dateFormatter: DateFormatter = {
+    let f = DateFormatter()
+     f.locale = Locale(identifier: "ko_kr")
+     return f
+}()
+
+extension Date {
+    var dateString: String {
+        dateFormatter.dateFormat = "M월 d일"
+        return dateFormatter.string(from: self)
+    }
+    
+    var timeString: String {
+        dateFormatter.dateFormat = "HH:00"
+        return dateFormatter.string(from: self)
+    }
+}
