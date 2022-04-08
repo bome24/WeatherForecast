60bba1e728ef2dbe361aa770e64d14f48e248b8e
bome24 <81296575+bome24@users.noreply.github.com>
2022-04-08 16:14:25 +0900

    use actual coordinate

--- WeatherForecast/Location/LocationManager.swift
+++ WeatherForecast/Location/LocationManager.swift
@@ -22,7 +22,19 @@ class LocationManager: NSObject {
     
     let manager: CLLocationManager
     
-    var currentLocationTitle: String?
+    var currentLocationTitle: String? {
+        didSet {
+            var userInfo = [AnyHashable: Any]()
+            if let location = currentLocation {
+                userInfo["location"] = location
+            }
+            
+            NotificationCenter.default.post(name: Self.currentLocationDidUpdate, object: nil, userInfo: userInfo)
+        }
+    }
+    var currentLocation: CLLocation?
+    
+    static let currentLocationDidUpdate = Notification.Name(rawValue: "currentLocationDidUpdate")
     
     func updateLocation() {
         let status: CLAuthorizationStatus
@@ -101,9 +113,10 @@ extension LocationManager: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations.last)
+        //print(locations.last)
         
         if let location = locations.last {
+            currentLocation = location
             updateAddress(from: location)
         }
     }
